a0bf2dacb450c281a854c241b704731f364aad5e
Evgeniy <95440463+MEvgeniy21@users.noreply.github.com>
2022-06-16 21:55:04 +0300

    task-1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,43 @@
+const refs = {
+  body: document.querySelector('body'),
+  btnStart: document.querySelector('[data-start]'),
+  btnStop: document.querySelector('[data-stop]'),
+};
 
+let idTimerInterval = null;
+
+btnBlock(refs.btnStop);
+refs.btnStart.addEventListener('click', onStart);
+refs.btnStop.addEventListener('click', onStop);
+
+function onStart() {
+  btnBlock(refs.btnStart);
+  btnUnlock(refs.btnStop);
+  if (!idTimerInterval) {
+    idTimerInterval = setInterval(() => {
+      refs.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+  }
+}
+
+function onStop() {
+  btnBlock(refs.btnStop);
+  btnUnlock(refs.btnStart);
+  idTimerInterval = clearInterval(idTimerInterval);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function btnBlock(btnEl) {
+  if (!btnEl.hasAttribute('disabled')) {
+    btnEl.setAttribute('disabled', '');
+  }
+}
+
+function btnUnlock(btnEl) {
+  if (btnEl.hasAttribute('disabled')) {
+    btnEl.removeAttribute('disabled');
+  }
+}
